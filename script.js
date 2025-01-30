447528daa1c8c5abb3ccc484e5284a4c2f1417d4
netodedanda <netodedanda2@gmail.com>
2025-01-30 01:42:49 -0300

    teste1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let btnMenu = document.getElementById('btnabrir');
 let menu = document.getElementById('menu-mobile');
 let overlay = document.querySelector('.overlay');
-let btnFechar = document.querySelector('.btnfechar'); // Botão para fechar o menu
+let btnFechar = document.querySelector('.btnfechar'); 
 
 btnMenu.addEventListener('click', () => {
     menu.classList.add('abrir-menu');
@@ -13,7 +13,6 @@ btnFechar.addEventListener('click', () => {
     overlay.classList.remove('ativo');
 });
 
-// Opcional: Fechar o menu ao clicar na overlay
 overlay.addEventListener('click', () => {
     menu.classList.remove('abrir-menu');
     overlay.classList.remove('ativo');
